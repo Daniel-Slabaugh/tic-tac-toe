ffefe94977dd668af2a43c1e9f83c758fd009c10
Daniel-Slabaugh <danielslabaugh@gmail.com>
2017-06-08 03:36:57 -0500

    fixed lines

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	var h = $("#b1").width();
+	$("#line-1").height(h);
+	$("#line-2").height(h);
+	$("#line-3").height(h);
+	$('.box').css({"padding":h/2});
+
+
+
+});
