f3c6dafc0f02316550641b7610fc2ed10b0ae5d9
Daniel-Slabaugh <danielslabaugh@gmail.com>
2017-06-08 09:50:10 -0500

    app.js changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 	var h = $("#b1").width();
 	$("#line-1").height(h);
@@ -5,6 +6,31 @@ $(document).ready(function() {
 	$("#line-3").height(h);
 	$('.box').css({"padding":h/2});
 
+	$("#b1").click(function(e) {
+		if(game.squares[1] == "x" || game.squares[1] == "o") {
+			alert("Someone already played in that spot");
+		} else {
+			game.squares[1] = game.turn;
+			changeTurn();
+			$("#b1").text('x');
+		}
 
+	})
 
 });
+
+
+var game = {
+	turn:"x",
+	movecount:0,
+	winner:"no",
+	squares:[]
+}
+
+function changeTurn() {
+	if(game.turn = "x") {
+		game.turn = "o"
+	} else {
+		game.turn = "x"
+	}
+}
